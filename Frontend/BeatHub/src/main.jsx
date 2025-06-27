5c59b2548afc2cba7f63ff6e5a452306c28fc75f
Prospi <manuprosperi04@gmail.com>
2025-06-27 16:20:16 +0000

    Initial frontend files and folders.

--- Frontend/BeatHub/src/main.jsx
+++ Frontend/BeatHub/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+ import { StrictMode, useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to the Home Page</h1>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+createRoot(document.getElementById('root')).render(<App />);
